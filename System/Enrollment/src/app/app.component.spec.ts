e75602cc3710736ad9f273e4b26da7d9d87bf035
Bakagan12 <duranonico@gmail.com>
2024-11-13 23:34:04 +0800

    Updated angular/cli

--- System/Enrollment/src/app/app.component.spec.ts
+++ System/Enrollment/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Enrollment' title`, () => {
+  it(`should have the 'enrollment' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Enrollment');
+    expect(app.title).toEqual('enrollment');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Enrollment');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, enrollment');
   });
 });
