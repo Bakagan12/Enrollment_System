a7a66bf0ed932f5d60fd240f5b0306d4a62ac727
Bakagan12 <duranonico@gmail.com>
2025-02-11 23:17:04 +0800

    updates

---
+++ System/backend/src/repository/studentRepository/get_all_students.ts
@@ -0,0 +1,16 @@
+import db from '../../util/database';
+import { Student } from '../../models/students';
+import { Persons } from '../../models/persons';
+
+export class getAllStudent{
+    static async saveInfo(person: Persons): Promise<any> {
+        return db('persons').insert({first_name: person.first_name});
+    }
+
+    // static async save(user: GenUser): Promise<any> {
+    //     return db.execute(
+    //         'INSERT INTO gen_users (username, password) VALUES (?, ?)',
+    //         [user.username, user.password]
+    //     );
+    // }
+}
